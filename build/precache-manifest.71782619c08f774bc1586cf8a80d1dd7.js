33deb6e543f98c2c4807a5b6b68a3414f60c54a8
liuliu <carrisa.liu@gmail.com>
2020-05-01 23:01:56 -0500

    Add build

---
+++ build/precache-manifest.71782619c08f774bc1586cf8a80d1dd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fd7f3650ec08a3d0ab5b58e97e94a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c20343d23e8f5a9b506",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "6a835af47eda19ca0d74",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2c20343d23e8f5a9b506",
+    "url": "/static/js/2.e040512e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e040512e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a835af47eda19ca0d74",
+    "url": "/static/js/main.dab25d32.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
