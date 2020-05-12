bb5c72a9f5dfd628fc17382f8ebe3a24bf9c724b
liuliu <carrisa.liu@gmail.com>
2020-05-11 21:58:25 -0500

    fixed accidently deleted docs

---
+++ docs/precache-manifest.b4e41f494ff700b8230796f3c4aabb6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0244bca425a1db9fe87e34fdb6ce4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078b6301d5f91f644dfb",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "2202f622b177a6ebc1dc",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "078b6301d5f91f644dfb",
+    "url": "/static/js/2.c6510afa.chunk.js"
+  },
+  {
+    "revision": "994da8d7cc4350594a6d76b205d24cf7",
+    "url": "/static/js/2.c6510afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2202f622b177a6ebc1dc",
+    "url": "/static/js/main.3a96c377.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
